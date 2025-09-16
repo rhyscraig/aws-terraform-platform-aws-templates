461c4d2d112571032b624cbf05f8fb53e7f28e19
Craig Hoad <choad@beyondtrust.com>
2025-09-16 12:53:53 +0100

    Update main.tf

--- terraform/control_tower/main.tf
+++ terraform/control_tower/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_controltower_landing_zone" "avm_landing_zone" {
   manifest_json = file("${path.module}/landing_zone_manifest.json")
   version       = "3.2"
